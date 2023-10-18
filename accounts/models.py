ccd43c1072a96b971a4942344c41421bcfb5b13d
Jonathan <jonathancornejox@gmail.com>
2023-10-18 14:05:43 -0700

    fixed spacing

--- accounts/models.py
+++ accounts/models.py
@@ -1,17 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+
 class Language(models.Model):
     name = models.CharField(max_length=150)
 
+
 class User(models.Model):
     first_name = models.CharField(max_length=150)
-    last_name =  models.CharField(max_length=150)
-    username =  models.CharField(max_length=150)
-    password =  models.CharField(max_length=150)
-    city =  models.CharField(max_length=150)
-    state =  models.CharField(max_length=150)
-    country =  models.CharField(max_length=150)
+    last_name = models.CharField(max_length=150)
+    username = models.CharField(max_length=150)
+    password = models.CharField(max_length=150)
+    city = models.CharField(max_length=150)
+    state = models.CharField(max_length=150)
+    country = models.CharField(max_length=150)
+
 
 class FriendsList(models.Model):
     sender = models.ForeignKey(
@@ -23,4 +27,3 @@ class FriendsList(models.Model):
         "User",
         related_name='recipient',
         on_delete=models.CASCADE)
-
