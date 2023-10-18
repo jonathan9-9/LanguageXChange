46eadcc00c16529bb646dea6d79c3921f6bdec0d
Jonathan <jonathancornejox@gmail.com>
2023-10-18 15:12:46 -0700

    modified admin register

--- accounts/admin.py
+++ accounts/admin.py
@@ -2,6 +2,21 @@ from django.contrib import admin
 from .models import Language, User, FriendsList
 # Register your models here.
 
-admin.site.register(Language)
-admin.site.register(User)
-admin.site.register(FriendsList)
+@admin.register(Language)
+class LanguageAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(FriendsList)
+class FriendsListAdmin(admin.ModelAdmin):
+    pass
+
+
+# admin.site.register(Language)
+# admin.site.register(User)
+# admin.site.register(FriendsList)
